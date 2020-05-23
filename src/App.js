527d6503f0bfd6486f06043c55d6e17c8663f5de
dilipanthony <dilippro@ymail.com>
2020-05-23 20:33:26 +0530

    Homepage layout added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from "./components/homepage/homepage-com.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
