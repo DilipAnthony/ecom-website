714639d9b1992089e422878b05d6eaedd69af99e
dilipanthony <dilippro@ymail.com>
2020-07-13 16:50:49 +0530

    Title has been changed

--- src/component/Category/category-com.jsx
+++ src/component/Category/category-com.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./category-style.css";
 
-const Category = ({match}) => {
-    console.log(match);
-    
-    return(
-        <div className="poda">
-            <h1>poda</h1>
-        </div>
-    )
-}
+const Category = ({ match }) => {
+  console.log(match);
+
+  return (
+    <div className="poda">
+      <h1>Test</h1>
+    </div>
+  );
+};
 
 export default Category;
