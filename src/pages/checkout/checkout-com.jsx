aafac856b6313879a2eaba6bd3583844c69dd89f
dilipanthony <dilippro@ymail.com>
2020-07-18 18:12:42 +0530

    checkout page and sign in page center

--- src/pages/checkout/checkout-com.jsx
+++ src/pages/checkout/checkout-com.jsx
@@ -1,12 +1,13 @@
-import React from "react"
-import CheckoutContent from "../../component/checkout-com/checkoutContent-com"
+import React from "react";
+import CheckoutContent from "../../component/checkout-com/checkoutContent-com";
+import "./checkout-style.css";
 
 const Checkout = () => {
-    return(
-        <div className="checkout">
-            <CheckoutContent />
-        </div>
-    )
-}
+  return (
+    <div className="checkout">
+      <CheckoutContent />
+    </div>
+  );
+};
 
 export default Checkout;
--- src/pages/signIn-SignUp-Page/SignIn-SignUp-Com.jsx
+++ src/pages/signIn-SignUp-Page/SignIn-SignUp-Com.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import "./SignIn-SignUp-style.css"
-import SignIn from "../../component/SIgnIn-com/SignIn-com.jsx"
-import SignUp from "../../component/signup/signup-com.jsx"
+import "./SignIn-SignUp-style.css";
+import SignIn from "../../component/SIgnIn-com/SignIn-com.jsx";
+import SignUp from "../../component/signup/signup-com.jsx";
 
-const Sign = ({status}) => {
-    return(
-        <div className="Sign-Parent">
-        <div className="first">
-            <SignIn status={status}/>
-            </div>
-            <div className="last">
-            <SignUp />
-            </div>
-        </div>
-    )
-}
+const Sign = ({ status }) => {
+  return (
+    <div className="Sign-Parent">
+      <div className="first">
+        <SignIn status={status} />
+      </div>
+      <div className="last">
+        <SignUp />
+      </div>
+    </div>
+  );
+};
 
 export default Sign;
